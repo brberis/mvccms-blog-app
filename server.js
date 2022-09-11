0b814b0dc2fff2fe2ab8ed8c7d86352bceb5f6de
Cristobal Barberis <cristobal@barberis.com>
2022-09-11 14:35:09 -0400

    post user

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(require('./controllers/'));
 
