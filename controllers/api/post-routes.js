b963f9847cd7467153d0d3d329bbf6ecc55cf5c5
Cristobal Barberis <cristobal@barberis.com>
2022-09-11 14:47:00 -0400

    create post with auth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -10,6 +11,19 @@ router.get('/', (req, res) => {
       'content',
       'created_at',
     ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.post_url,
+    user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
