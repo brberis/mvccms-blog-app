86fccbeb5a141ec665711e2134430d723d8cb42b
Cristobal Barberis <cristobal@barberis.com>
2022-09-10 20:27:05 -0400

    bcrypt user password

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create user model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields for user model
 User.init(
@@ -31,6 +37,20 @@ User.init(
       validate: {
         len: [4]
       }
+    }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
     sequelize,
     timestamps: false,
@@ -38,8 +58,6 @@ User.init(
     underscored: true,
     modelName: 'user'
   }
-
-  
 );
 
 module.exports = User;
